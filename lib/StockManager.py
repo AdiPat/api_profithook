2140181c04cb8fcc04a489e34dbb348a0549d507
Adi_Pat <adipat247360@gmail.com>
2019-09-04 20:08:12 +0530

    Replace MySQL with SQLite for storing meta data

--- lib/StockManager.py
+++ lib/StockManager.py
@@ -5,61 +5,21 @@ import base
 ##
 
 import scraper
-import pymysql
-#import sqlite3
+import sqlite3
+
+STOCKDB = 'stocks.db'
 
 
 class StockManager:
     ## Initializes catalog
     def __init__(self, conf):
-        #print(conf)
-        self.conn = pymysql.connect(host='localhost', user=conf['user'], password=conf['password'], db=conf['db'], charset='utf8mb4', cursorclass=pymysql.cursors.DictCursor)
         self.driver = scraper.init_webdriver('normal')
-        #self.updateCatalog(self, index="Z");        
-                
-    ## Updates the catalog db with latest data
-    ## catalog(id,name,url)
-    ##
-    @staticmethod
-    def updateCatalog(self, index=scraper.DEFAULT_CATALOG_INDEX):
-        # Pull data using scraper
-        catalog = scraper.fetchAll(scraper.getSearchResults(index))
-        # Initialize cursor
-        cur = self.conn.cursor()
-        # fill db
-        #cur.execute('ALTER TABLE catalog ADD sc_id varchar(10)')
-        cur.conn.commit()
-        sql = "INSERT INTO catalog (companyKey, companyId, companyName, url, sc_id) VALUES (%s, %s, %s, %s, %s)"
-        for item in catalog:
-            #print("\nINSERTING ({0}, {1}, {2}, {3})".format(item['companyKey'], item['companyId'], item['name'], item['link']))
-            
-            ## Check if old data is present
-            cur.execute("SELECT * FROM catalog WHERE companyKey='{0}'".format(item['companyKey']))
-            oldData = cur.fetchone()
-            if(oldData):
-            #if(False):
-                ## Exists
-                print("{0} data exists. Checking if data is up to date.".format(item['companyKey']))
-                ## Check if up to date
-                if(not (oldData['companyId'] == item['companyId'] and oldData['companyName'] == item['name'] and oldData['url'] == item['link']) and oldData['sc_id'] == item['sc_id']):
-                    # UPDATE
-                    print("{0} data is outdated. Updating. ".format(item['companyKey']))
-                    cur.execute("UPDATE catalog SET companyId='{0}' , companyName='{1}' , url='{2}' WHERE companyKey = '{3}'".format(item['companyId'], item['name'], item['link'], item['companyKey'],item['sc_id']))
-                    self.conn.commit()
-            else:
-                ## INSERT 
-                cur.execute(sql, (item['companyKey'], item['companyId'], item['name'], item['link']), item['sc_id'])
-                self.conn.commit()
-        cur.close()
-
-    def getCatalog(self):
-        return ['hullo']
-    
     
     ## Returns stock data 
     def getStockData(self, id):
         data = []
-        cur = self.conn.cursor()
+        conn = sqlite3.connect(STOCKDB)
+        cur = conn.cursor()
         cur.execute("SELECT * FROM catalog WHERE companyId='{0}'".format(str(id)))
         stockInfo =  cur.fetchall()
         for row in stockInfo:
@@ -72,7 +32,7 @@ class StockManager:
             ## PULLS ALL DATA FROM MONEYCONTROL
             scrapedData = scraper.fetchStockData(url)
             data.append(scrapedData)
-        cur.close()
+        conn.close()
         return data
     
     ## Returns search results for a tag
@@ -81,7 +41,8 @@ class StockManager:
         tags = base.parseSearchQuery(query)
         if(len(tags) == 0):
             return results
-        cur = self.conn.cursor()        
+        conn = sqlite3.connect(STOCKDB)
+        cur = conn.cursor()        
         ## Fetch all results based on tags
         alias = 'table'
         sql = "SELECT * from %inner where companyName like '%{%c}%' or companyId like '%{%c}%'"
@@ -93,13 +54,14 @@ class StockManager:
                 curRes = cur.fetchall()
                 res.append(curRes)
         #print(res)
-        cur.close()
+        conn.close()
         return res
     
     ## Returns all results which match companyId
     def company(self, companyId="", companyKey = ""):
         results = []
-        cur = self.conn.cursor()
+        conn = sqlite3.connect(STOCKDB)
+        cur = conn.cursor()
         sql = "SELECT * FROM catalog WHERE company{%s}='{0}'"
         if(companyKey):
             sql = sql.replace('{%s}', 'Key').format(companyKey)
@@ -109,7 +71,7 @@ class StockManager:
             return None
         cur.execute(sql)
         results = cur.fetchall()
-        cur.close()
+        conn.close()
         return results
     
     #
